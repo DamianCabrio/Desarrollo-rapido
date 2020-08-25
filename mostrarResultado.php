73b59d7fa77a808d884aa73e1acb9df616d2b5a7
Damian Cabrio <45396062+DamianCabrio@users.noreply.github.com>
2020-08-25 19:48:39 -0300

    Andando

---
+++ mostrarResultado.php
@@ -0,0 +1,39 @@
+<?php
+
+function calculaedad($fechanacimiento){
+    list($ano,$mes,$dia) = explode("-",$fechanacimiento);
+    $ano_diferencia  = date("Y") - $ano;
+    $mes_diferencia = date("m") - $mes;
+    $dia_diferencia   = date("d") - $dia;
+    if ($dia_diferencia < 0 || $mes_diferencia < 0)
+        $ano_diferencia--;
+    return $ano_diferencia;
+}
+
+foreach ($_POST["fechas"] as $fecha){
+    $grupoRiesgo = 0;
+    $edad = calculaedad($fecha);
+    if($edad >= 60){
+        $grupoRiesgo++;
+    }
+}
+
+$porcentageGrupoRiesgo = ($grupoRiesgo * 100) / count($_POST["fechas"]);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>El porcentaje de personas en el grupo de riesgo es:</h2>
+    <?= $porcentageGrupoRiesgo; ?>
+    <h1></h1>
+</body>
+</html>
